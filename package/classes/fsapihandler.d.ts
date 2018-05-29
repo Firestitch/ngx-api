ae129709cff80960c5d175f14043724d3f60aadd
Raymond <ray@firestitch.com>
2018-05-29 11:55:57 -0400

    updated tools ignored package

--- package/classes/fsapihandler.d.ts
+++
@@ -1,7 +0,0 @@
-import { HttpClient, HttpRequest, HttpHeaderResponse, HttpProgressEvent, HttpResponse, HttpSentEvent, HttpUserEvent } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-export declare class FsApiHandler {
-    private http;
-    constructor(http: HttpClient);
-    handle(request: HttpRequest<any>): Observable<HttpSentEvent | HttpHeaderResponse | HttpProgressEvent | HttpResponse<{}> | HttpUserEvent<{}>>;
-}
