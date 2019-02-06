1d37f4451b8e6c42c04031a943b67fe5d686fdc3
Eugene Kubesh <e.kubesh@wis.eu.com>
2019-02-06 10:22:56 +0300

    fixed params encoding

---
+++ src/classes/params-encoder.ts
@@ -0,0 +1,20 @@
+import { HttpUrlEncodingCodec } from '@angular/common/http';
+
+
+export default class CustomParamsEncoder extends HttpUrlEncodingCodec {
+  encodeKey(key: string): string {
+    return encodeURIComponent(key);
+  }
+
+  encodeValue(value: string): string {
+    return encodeURIComponent(value);
+  }
+
+  decodeKey(key: string): string {
+    return decodeURIComponent(key);
+  }
+
+  decodeValue(value: string): string {
+    return decodeURIComponent(value);
+  }
+}
