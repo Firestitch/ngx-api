7d277c1d1bfca13c51ba5d29f8aadc178c0fb190
Raymond Gigliotti <ray@firestitch.com>
2024-07-08 15:26:25 -0400

    included body data in body interceptor

--- src/app/interceptors/body.interceptor.ts
+++ src/app/interceptors/body.interceptor.ts
@@ -27,8 +27,12 @@ export class BodyInterceptor implements HttpInterceptor {
   }
 
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if(this._config.encoding === 'formdata') {
-      const body = objectToFormData(this._data);
+    if(this._data) {
+      let body = this._data;
+      if(this._config.encoding === 'formdata') {
+        body = objectToFormData(this._data);
+      }
+
       req = req.clone({ body });
     }
 
